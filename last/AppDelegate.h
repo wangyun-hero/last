585de7906f2a0beddd9dc7af45cc7dc5a87a7129
wangyun-hero <wangyun120411@163.com>
2017-07-03 23:39:56 +0800

    Initial Commit

---
+++ last/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  last
+//
+//  Created by 王云 on 2017/7/3.
+//  Copyright © 2017年 王云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ last/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  last
+//
+//  Created by 王云 on 2017/7/3.
+//  Copyright © 2017年 王云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
